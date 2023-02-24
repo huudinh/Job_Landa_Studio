54e174176a9a88cdb621b8b06f1be2233058411e
huudinh <dinhndvn@yahoo.com>
2023-02-24 19:16:53 +0700

    update

--- Category/breadcrumb_dls_1_0_0/breadcrumb_dls_1_0_0.php
+++
@@ -1,4 +0,0 @@
-<div class="breadcrumb_1_1_0">
-    <?php //if ( function_exists('yoast_breadcrumb') ) {yoast_breadcrumb('<p id="">','</p>');} ?>
-    <?php if (function_exists('rank_math_the_breadcrumbs')) rank_math_the_breadcrumbs(); ?>
-</div>
---
+++ footer.php
@@ -0,0 +1,6 @@
+        <?php get_template_part('Module/module_footer'); ?>   
+    </main>
+    <?php wp_footer(); ?>
+</body>
+
+</html>
